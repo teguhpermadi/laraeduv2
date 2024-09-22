4b4cc57a9d52dc6b75a04223c42c57f4089c4109
teguhpermadi <tguh.permadi@gmail.com>
2024-09-23 04:26:01 +0700

    feat: teacher userable

--- app/Filament/Resources/TeacherResource.php
+++ app/Filament/Resources/TeacherResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\TeacherResource\Pages;
 use App\Filament\Resources\TeacherResource\RelationManagers;
+use App\Jobs\UserableJob;
 use App\Models\Teacher;
 use Filament\Forms;
 use Filament\Forms\Components\FileUpload;
@@ -12,11 +13,16 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Userable;
 
 class TeacherResource extends Resource
 {
@@ -66,14 +72,15 @@ class TeacherResource extends Resource
                 TextColumn::make('gender')
                     ->label(__('teacher.list.gender'))
                     ->sortable(),
-                ImageColumn::make('signature')
-                    ->label(__('teacher.list.signature'))
-                    ->size('50')
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Action::make('Userable')
+                    ->action(function (Teacher $teacher) {
+                        self::userable($teacher);
+                    }),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
@@ -98,5 +105,35 @@ class TeacherResource extends Resource
             'create' => Pages\CreateTeacher::route('/create'),
             'edit' => Pages\EditTeacher::route('/{record}/edit'),
         ];
+    }
+
+    public static function userable($data)
+    {
+        $name = $data->name;
+        $userable_id = $data->id;
+        $userable_type = Teacher::class;
+
+        $user = User::firstOrCreate(
+            [
+                'email' => Str::replace(' ', '', $name) . '@laraedu.com',
+            ],
+            [
+                'name' => $name,
+                'email' => Str::replace(' ', '', $name) . '@laraedu.com',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $userable = Userable::firstOrCreate(
+            [
+                'userable_id' => $userable_id,
+                'userable_type' => $userable_type,
+            ],
+            [
+                'user_id' => $user->id,
+                'userable_id' => $userable_id,
+                'userable_type' => $userable_type
+            ]
+        );
     }
 }
---
+++ database/migrations/2024_09_20_051822_create_userables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('userables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('userable_id');
+            $table->string('userable_type');
+            $table->timestamps();
+            
+            $table->unique(['userable_id', 'userable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('userables');
+    }
+};
