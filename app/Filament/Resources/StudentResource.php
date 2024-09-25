024bd032572225efd44b6138bdaa74c8422922ec
teguhpermadi <tguh.permadi@gmail.com>
2024-09-25 19:31:01 +0700

    feat: student resource

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getModelLabel(): string
+    {
+        return __('student.student');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label(__('student.name'))
+                    ->required(),
+                TextInput::make('nick_name')
+                    ->label(__('student.nick_name')),
+                Select::make('gender')
+                    ->label(__('student.gender'))
+                    ->options([
+                        'laki-laki' => 'laki-laki',
+                        'perempuan' => 'perempuan'
+                    ])
+                    ->required(),
+                TextInput::make('city_born')
+                    ->label(__('student.city_born')),
+                DatePicker::make('birthday')
+                    ->label(__('student.birthday')),
+                Toggle::make('active')
+                    ->label(__('student.active')),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('student.name')),
+                TextColumn::make('gender')
+                    ->label(__('student.gender')),
+                TextColumn::make('nis')
+                    ->label(__('student.nis')),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ lang/id/student.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'student' => 'siswa',
+    'name' => 'nama lengkap',
+    'nick_name' => 'nama panggilan',
+    'gender' => 'jenis kelamin',
+    'city_born' => 'kota kelahiran',
+    'birthday' => 'tanggal lahir',
+    'active' => 'aktif',
+    'nis' => 'nis',
+    'nisn' => 'nisn',
+];
