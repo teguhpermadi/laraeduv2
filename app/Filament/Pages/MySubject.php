24459cd2aed7a32b4991f191269c0f844e962ef8
teguhpermadi <tguh.permadi@gmail.com>
2024-10-09 11:50:48 +0700

    feat(custom page): my subject

---
+++ app/Filament/Pages/MySubject.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\TeacherSubject;
+use Filament\Pages\Page;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+
+class MySubject extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.my-subject';
+
+    public static function getNavigationLabel(): string
+    {
+        return __('my-subject.my-subject');
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+        ->query(TeacherSubject::query()->mySubject())
+        ->columns([
+            TextColumn::make('subject.name')
+                ->label(__('my-subject.subject'))
+                ->searchable(),
+            TextColumn::make('grade.name')
+                ->label(__('my-subject.grade'))
+                ->searchable(),
+        ])
+        ->filters([
+            // ...
+        ])
+        ->actions([
+            Action::make('assesment'),
+        ]);
+    }
+}
---
+++ lang/id/my-subject.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'my-subject' => 'Pelajaran Ku',
+    'subject' => 'Mata Pelajaran',
+    'grade' => 'Kelas',
+];
