739fcb1cd20ee2584e85d3bd89556ee4b9408f26
teguhpermadi <tguh.permadi@gmail.com>
2024-11-06 21:27:35 +0700

    feat: project theme

---
+++ database/seeders/ProjectThemeSeeder.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProjectTheme;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectThemeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                "name" => "Aku Sayang Bumi",
+                "level" => "paud"
+            ],
+            [
+                "name" => "Aku Cinta Indonesia",
+                "level" => "paud"
+            ],
+            [
+                "name" => "Kita Semua Bersaudara",
+                "level" => "paud"
+            ],
+            [
+                "name" => "Imajinasi dan Kreativitasku",
+                "level" => "paud"
+            ],
+            [
+                "name" => "Gaya Hidup Berkelanjutan",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Kearifan Lokal",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Bhinneka Tunggal Ika",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Bangunlah Jiwa dan Raganya",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Rekayasa dan Teknologi",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Kewirausahaan",
+                "level" => "sd"
+            ],
+            [
+                "name" => "Gaya Hidup Berkelanjutan",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Kearifan Lokal",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Bhinneka Tunggal Ika",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Bangunlah Jiwa dan Raganya",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Suara Demokrasi",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Rekayasa dan Teknologi",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Kewirausahaan",
+                "level" => "smp"
+            ],
+            [
+                "name" => "Gaya Hidup Berkelanjutan",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Kearifan Lokal",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Bhinneka Tunggal Ika",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Bangunlah Jiwa dan Raganya",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Suara Demokrasi",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Rekayasa dan Teknologi",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Kewirausahaan",
+                "level" => "sma"
+            ],
+            [
+                "name" => "Gaya Hidup Berkelanjutan",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Kearifan Lokal",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Bhinneka Tunggal Ika",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Bangunlah Jiwa dan Raganya",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Suara Demokrasi",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Rekayasa dan Teknologi",
+                "level" => "smk"
+            ],
+            [
+                "name" => "Kebekerjaan",
+                "level" => "smk"
+            ]
+        ];
+
+        ProjectTheme::insert($data);
+    }
+}
