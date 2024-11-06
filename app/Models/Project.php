6a53d434643a589e670b26208dd198333b22bed4
teguhpermadi <tguh.permadi@gmail.com>
2024-11-06 21:48:42 +0700

    feat: project -m

---
+++ app/Models/Project.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\AcademicYearScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'academic_year_id',
+        'grade_id',
+        'teacher_id',
+        'name',
+        'description',
+        'phase',
+    ];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new AcademicYearScope);
+    }
+
+    public function academic()
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year_id');
+    }
+
+    public function teacher()
+    {
+         return $this->belongsTo(Teacher::class);
+    }
+    
+    public function grade()
+    {
+         return $this->belongsTo(Grade::class);
+    }
+
+    public function projectTarget()
+    {
+        return $this->hasMany(ProjectTarget::class);
+    }
+
+    public function note()
+    {
+        return $this->hasMany(ProjectNote::class);
+    }
+
+    public function scopeMyProject(Builder $query, $teacher_id = null):void
+    {
+        if(is_null($teacher_id)){
+            $teacher_id = auth()->user()->userable->userable_id;
+        }
+
+        $query->where('teacher_id', $teacher_id);
+    }
+}
