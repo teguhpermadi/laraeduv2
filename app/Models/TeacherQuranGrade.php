6a93f89e8ea95704c284b959d7588729f7ca1d90
teguhpermadi <tguh.permadi@gmail.com>
2024-11-10 09:50:58 +0700

    feat: teacher quran grade

---
+++ app/Models/TeacherQuranGrade.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\AcademicYearScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TeacherQuranGrade extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'academic_year_id',
+        'teacher_id',
+        'quran_grade_id',
+    ];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new AcademicYearScope);
+    }
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(Teacher::class);
+    }   
+
+    public function quranGrade(): BelongsTo
+    {
+        return $this->belongsTo(QuranGrade::class);
+    }   
+
+    public function academicYear(): BelongsTo
+    {
+        return $this->belongsTo(AcademicYear::class);
+    }
+}
