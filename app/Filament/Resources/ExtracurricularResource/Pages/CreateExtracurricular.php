569fc0a6eb8e1b9466799c6e232398f32f452261
teguhpermadi <tguh.permadi@gmail.com>
2024-11-10 08:14:11 +0700

    fix: extracurricular

--- app/Filament/Resources/ExtracurricularResource/Pages/CreateExtracurricular.php
+++ app/Filament/Resources/ExtracurricularResource/Pages/CreateExtracurricular.php
@@ -3,10 +3,29 @@
 namespace App\Filament\Resources\ExtracurricularResource\Pages;
 
 use App\Filament\Resources\ExtracurricularResource;
+use App\Models\Student;
+use App\Models\StudentExtracurricular;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateExtracurricular extends CreateRecord
 {
     protected static string $resource = ExtracurricularResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        // buat dulu extracurricular
+        $extracurricular = static::getModel()::create($data);
+
+        // jika is_required true maka tambahkan semua data siswa ke table student_extracurricular
+        if ($data['is_required']) {
+            $students = Student::all();
+            foreach ($students as $student) {
+                StudentExtracurricular::create(['student_id' => $student->id, 'extracurricular_id' => $extracurricular->id]);
+            }
+        }
+        
+        return $extracurricular;
+    }
 }
--- app/Filament/Resources/ExtracurricularResource/Pages/EditExtracurricular.php
+++ app/Filament/Resources/ExtracurricularResource/Pages/EditExtracurricular.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\ExtracurricularResource\Pages;
 
 use App\Filament\Resources\ExtracurricularResource;
+use App\Models\Student;
+use App\Models\StudentExtracurricular;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditExtracurricular extends EditRecord
 {
@@ -13,7 +16,35 @@ class EditExtracurricular extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\DeleteAction::make(),
+            Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        // jika is_required true maka tambahkan semua data siswa ke table student_extracurricular
+        if ($data['is_required']) {
+            $students = Student::all();
+            foreach ($students as $student) {
+                StudentExtracurricular::create([
+                    'academic_year_id' => session('academic_year_id'),
+                    'student_id' => $student->id, 
+                    'extracurricular_id' => $record->id
+                ]);
+            }
+        } else {
+            // jika is_required false maka hapus semua data siswa dari table student_extracurricular
+            StudentExtracurricular::where('extracurricular_id', $record->id)->delete();
+        }
+
+        $record->update($data);
+ 
+        return $record;
+    }
+
+    // redirec ke halaman edit extracurricular
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('edit', ['record' => $this->getRecord()]);
     }
 }
