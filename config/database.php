af17b3f3e3afe6ec998e152aa4afea5721fbe1d6
teguhpermadi <tguh.permadi@gmail.com>
2024-12-04 04:59:13 +0700

    feat: log-viewer install

--- config/database.php
+++ config/database.php
@@ -62,6 +62,26 @@ return [
             ]) : [],
         ],
 
+        'laraedu' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT_LARAEDU', '3306'),
+            'database' => env('DB_DATABASE_LARAEDU', 'laravel'),
+            'username' => env('DB_USERNAME_LARAEDU', 'root'),
+            'password' => env('DB_PASSWORD_LARAEDU', ''),
+            'unix_socket' => env('DB_SOCKET_LARAEDU', ''),
+            'charset' => env('DB_CHARSET_LARAEDU', 'utf8mb4'),
+            'collation' => env('DB_COLLATION_LARAEDU', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
