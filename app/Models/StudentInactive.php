d25b02767be292b694d57121e09eb77865ee8335
teguhpermadi <tguh.permadi@gmail.com>
2024-12-05 19:08:59 +0700

    feat: student inactive -mfs

---
+++ app/Models/StudentInactive.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentInactive extends Model
+{
+    use HasFactory;
+    use HasUlids;
+
+    // Menentukan bahwa kita tidak menggunakan auto-increment
+    public $incrementing = false;
+
+    // Tipe primary key adalah string (karena ULID berupa string)
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'academic_year_id',
+        'student_id',
+    ];
+
+    public function academic()
+    {
+        return $this->belongsTo(AcademicYear::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
