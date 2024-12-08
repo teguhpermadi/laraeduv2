bb3c3686bfffa325d367d7ef6c48bfb9b04e9a2b
teguhpermadi <tguh.permadi@gmail.com>
2024-12-08 14:47:59 +0700

    feat: leger preview my grade

--- app/Livewire/LegerPreviewMyGrade.php
+++ app/Livewire/LegerPreviewMyGrade.php
@@ -10,13 +10,68 @@ use Livewire\Component;
 
 class LegerPreviewMyGrade extends Component
 {
-    public $myGrade;
-    public $subjects;
+    public $myGrade, $subjects;
+    public $data;
 
     public function mount()
     {
         $myGrade = TeacherGrade::myGrade()->mySubject()->get();
+        
         $this->myGrade = $myGrade;
+
+        $data = [];
+
+        foreach ($myGrade as $grade) { // setiap grade
+
+
+            $no = 1;
+            foreach ($grade->studentGrade as $student) { // setiap student
+
+                $data[$grade->id][$student->student_id] = [
+                    'no' => $no++,
+                    'student' => $student->student->toArray(),
+                ];
+
+                foreach ($grade->grade->teacherSubject as $subject) { // setiap subject
+                    $leger = $student->student->leger()
+                        ->where('category', 'full_semester')
+                        ->where('subject_id', $subject->subject_id)
+                        ->first();
+
+                    if ($leger) {
+                        $data[$grade->id][$student->student_id]['leger'][] = $leger->toArray();
+                    } else {
+                        $data[$grade->id][$student->student_id]['leger'][] = null;
+                    }
+                }
+
+                $legers = $student->student->leger()
+                    ->where('category', 'full_semester')
+                    ->get();
+
+                // buatkan jumlah dari setiap nilai
+                $data[$grade->id][$student->student_id]['total'] = $legers->sum('score');
+                // buatkan rata-rata dari setiap nilai
+                $data[$grade->id][$student->student_id]['average'] = $legers->avg('score');
+                // buatkan ranking dari setiap nilai
+            }
+
+            // pada setiap grade, urutkan berdasarkan total score
+            $data[$grade->id] = collect($data[$grade->id])->sortByDesc('total')->values();
+
+            // pada setiap grade, buatkan ranking berdasarkan total score
+            $data[$grade->id] = $data[$grade->id]->map(function ($item, $index) {
+                $item['ranking'] = $index + 1;
+                return $item;
+            });
+
+            // kembalikan data berdasarkan student_id asc
+            $data[$grade->id] = collect($data[$grade->id])->sortBy('student.id')->values();
+
+        }
+
+        // dd($data);
+        $this->data = $data;
     }
 
     public function render()
--- resources/views/livewire/leger-preview-my-grade.blade.php
+++ resources/views/livewire/leger-preview-my-grade.blade.php
@@ -30,40 +30,36 @@
         </div>
 
         
-        <table class="text-black w-full max-w-2xl mx-auto border-collapse border border-gray-300">
+        <table class="text-black w-full border-collapse border border-gray-300">
             <thead class="bg-gray-200">
                 <tr>
-                    <th class="border border-gray-300 px-4 py-2">No</th>
-                    <th class="border border-gray-300 px-4 py-2">Nama</th>
+                    <th class="border border-gray-300 p-3 text-center">No</th>
+                    <th class="border border-gray-300 p-3 text-center">NIS</th>
+                    <th class="border border-gray-300 p-3 text-left">Nama</th>
                     @foreach ($grade->grade->teacherSubject as $subject)
-                        <th class="border border-gray-300 px-4 py-2">{{ $subject->subject->code }}</th>
+                        <th class="border border-gray-300 p-3 text-center">{{ $subject->subject->code }}</th>
                     @endforeach
+                    <th class="border border-gray-300 p-3">Jumlah Nilai</th>
+                    <th class="border border-gray-300 p-3">Rata-rata</th>
+                    <th class="border border-gray-300 p-3">Rangking</th>
                 </tr>
             </thead>
             <tbody>
-                @php
-                    $no = 1;
-                @endphp
-                @foreach ($grade->studentGrade as $student)
-                    <tr class="{{ $loop->even ? 'bg-gray-100' : 'bg-white' }}">
-                        <td class="border border-gray-300 px-4 py-2">{{ $no++ }}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{ $student->student->name }}</td>
-                        @foreach ($grade->grade->teacherSubject as $subject)
-                            @php
-                                $leger = $student->student
-                                    ->leger()
-                                    ->where('category', 'full_semester')
-                                    ->where('subject_id', $subject->subject_id)
-                                    ->first();
-                            @endphp
-
+                @foreach ($data[$grade->id] as $student)
+                    <tr class="border odd:bg-gray-100">
+                        <td class="p-3 text-center">{{ $student['no'] }}</td>
+                        <td class="p-3 text-left">{{ $student['student']['nis'] }}</td>
+                        <td class="p-3 text-left">{{ $student['student']['name'] }}</td>
+                        @foreach ($student['leger'] as $leger)
                             @if ($leger)
-                                <td class="border border-gray-300 px-4 py-2">{{ $leger->score }}</td>
+                                <td class="p-3 text-center">{{ $leger['score'] }}</td>
                             @else
-                                <td class="border border-gray-300 px-4 py-2">-</td>
+                                <td class="p-3 text-center">-</td>
                             @endif
-
                         @endforeach
+                        <td class="p-3 text-center">{{ $student['total'] }}</td>
+                        <td class="p-3 text-center">{{ $student['average'] }}</td>
+                        <td class="p-3 text-center">{{ $student['ranking'] }}</td>
                     </tr>
                 @endforeach
             </tbody>
